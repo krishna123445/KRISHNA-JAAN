4ef19c5b538088f8bd4dcaff56e89e199c1e698f
RAVI KUMAR DAVANG <167332663+ravi-with-kumarr@users.noreply.github.com>
2024-12-06 21:30:51 +0530

    Create Krishna.js

---
+++ Priyansh/commands/Krishna.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "prem aa gya",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "riya", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "Prem",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("owner")==0 || event.body.indexOf("Owner")==0 || event.body.indexOf("Boss")==0 || event.body.indexOf("Malik")==0) {
+		var msg = {
+				body: "💖💖😍☞𝕂ℝ𝕀𝕊ℍℕ𝔸 𝔹𝕆𝕋☜😍💖💖✧═════════•❁❀❁•═════════✧           💖   💖 ✧═════════•❁❀❁•═════════✧   💝🥀𝐎𝐖𝐍𝐄𝐑 :-  ☞💖👑 𝗞𝗥𝗜𝗦𝗛𝗡𝗔 👑💖☜ ✧═════════•❁❀❁•═════════✧   💝🥀𝐎𝐖𝐍𝐄𝐑 :-☞💖👑 𝙺𝚁𝙸𝚂𝙷𝙽𝙰 👑💖☜ ✧═════════•❁❀❁•═════════✧                     💖💖☞︎𝙼𝙰𝚂𝚃𝙸 𝙺𝙰𝚁𝙾 𝙼𝙴𝚁𝙴 𝚂𝙰𝚃𝙷☜︎💖💖✧═════════•❁❀❁•═════════✧   𝙿𝙰𝙶𝙰𝙻 𝚃𝚄𝙼𝙰𝚁𝙰 𝙶𝙾𝙻𝚄 𝙼𝙾𝙻𝚄 𝙱𝙰𝙱𝚄 𝙷𝚄 𝙼𝙴✧═════════•❁❀❁•═════════✧    💖𝗢𝗪𝗡𝗘𝗥÷ 𝗠𝗔𝗗𝗘 𝗕𝗬 𝐊𝐑𝐈𝐒𝐇𝐍𝐀💝💝💝 ✧═════════•❁❀❁•═════════✧      💖𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 𝗜𝗗 :- 👇 https://www.facebook.com/profile.php?id=61568216591260&mibextid=ZbWKwL ✧═════════•❁❀❁•═════════✧ किसी भी तरह की हेल्प के लिए कांटेक्ट करे 👉 [+91 8094828237]",
+        attachment: fs.createReadStream(__dirname + `/noprefix/riya2.png.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("💝", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }

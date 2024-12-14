f08be8e21bd61d723bd83715e9314d4aa35da13b
RAVI KUMAR DAVANG <167332663+ravi-with-kumarr@users.noreply.github.com>
2024-12-14 21:43:28 +0530

    Update mention.js

--- Priyansh/commands/mention.js
+++ Priyansh/commands/mention.js
@@ -2,18 +2,18 @@ module.exports.config = {
   name: "goiadmin",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝐊𝐑𝐈𝐒𝐇𝐍𝐀",
   description: "Bot will rep ng tag admin or rep ng tagbot ",
   commandCategory: "Other",
   usages: "",
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100037743553265") {
-    var aid = ["100037743553265"];
+  if (event.senderID !== "61568216591260") {
+    var aid = ["61568216591260"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["Wo Busy H mujhe Bolo Kya Bolna H?", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy hoga", "Priyansh Toh Chala gaya"];
+      var msg = ["Wo Busy H mujhe Bolo Kya Bolna H?", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy hoga", "𝐊𝐑𝐈𝐒𝐇𝐍𝐀 𝐉𝐈 𝐓𝐎 𝐂𝐇𝐀𝐋𝐄 𝐆𝐘𝐄 🤔"];
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
     }}

5439527d4de2714712ccab425347912fa8d2a03a
RAVI KUMAR DAVANG <167332663+ravi-with-kumarr@users.noreply.github.com>
2024-12-15 07:38:08 +0530

    Update relationship.js

--- Priyansh/commands/relationship.js
+++ Priyansh/commands/relationship.js
@@ -1,33 +1,26 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "Dance",
-    version: "1.1.1",
+	name: "prem aa gya",
+    version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Krishna", 
-	description: "Just Respond",
+	credits: "riya", 
+	description: "hihihihi",
 	commandCategory: "no prefix",
+	usages: "Prem",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	let react = event.body.toLowerCase();
-	if(react.includes("relationship") ||
-     react.includes("Relationship") || react.includes("RELATIONSHIP") || react.includes("Relationship") ||
-react.includes("RELATIONSHIP") ||
-react.includes("rElAtIoNsHiP")) {
+	if (event.body.indexOf("owner")==0 || event.body.indexOf("Owner")==0 || event.body.indexOf("Boss")==0 || event.body.indexOf("OWNER")==0) {
 		var msg = {
-				body: `‎🌿🍒🌸𝗘𝗞 𝗗𝗨𝗦𝗥𝗘 𝗞𝗜 𝗝𝗔𝗔𝗡🌸🍒🌿
-\n✧═════════•❁❀❁•═════════✧\n
-🍒🍓☞︎ [ 𝗞𝗥𝗜𝗦𝗛𝗡𝗔 𝗞𝗜 𝗝𝗔𝗔𝗡 ] ☜︎🍓🍒\n✧═════════•❁❀❁•═════════✧\n💝💕💕😍𝗜 𝗟𝗢𝗩𝗘 𝗬𝗢𝗨 𝗟𝗔𝗟𝗟𝗔😚😍😘\n✧═════════•❁❀❁•═════════✧\n
- •||• #ρყαɾ ʋσ _ɳԋι❌ ʝιʂɱҽ👉#Attitude 😒σɾ #Ego😏ԋσ, •||•
-•||• #ρყαɾ😍 _ƚσ ʋσ ԋαι #ʝιʂɱ _ҽƙ “ɾσƚԋαɳ”
-•||• ɱҽ#Expert 😘ԋσ ƚσ#ԃυʂɾα •|||• ɱαɳαɳҽ ɱҽ  👉#Perfect❤️️ ԋσ. •||•`,attachment: fs.createReadStream(__dirname + `/wall/jaan.jpg`)
+				body: "🌿🍒🌸𝗘𝗞 𝗗𝗨𝗦𝗥𝗘 𝗞𝗜 𝗝𝗔𝗔𝗡🌸🍒🌿\n✧═════════•❁❀❁•═════════✧\n🍒🍓☞︎ [ 𝗞𝗥𝗜𝗦𝗛𝗡𝗔 𝗞𝗜 𝗝𝗔𝗔𝗡 ] ☜︎🍓🍒\n✧═════════•❁❀❁•═════════✧\n💝💕💕😍𝗜 𝗟𝗢𝗩𝗘 𝗬𝗢𝗨 𝗟𝗔𝗟𝗟𝗔😚😍😘\n✧═════════•❁❀❁•═════════✧\n•||• #ρყαɾ ʋσ _ɳԋι❌ ʝιʂɱҽ👉#Attitude 😒σɾ #Ego😏ԋσ, •||••||• #ρყαɾ😍 _ƚσ ʋσ ԋαι #ʝιʂɱ _ҽƙ “ɾσƚԋαɳ”•||• ɱҽ#Expert 😘ԋσ ƚσ#ԃυʂɾα •|||• ɱαɳαɳҽ ɱҽ  👉#Perfect❤️️ ԋσ. •||•",
+        attachment: fs.createReadStream(__dirname + `/wall/jaan.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
+    api.setMessageReaction("💝", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+	}

e9730cc24db9af84cdc7ce422f1a9df691930f12
RAVI KUMAR DAVANG <167332663+ravi-with-kumarr@users.noreply.github.com>
2024-12-18 09:02:35 +0530

    Update 0_boss.js

--- Priyansh/commands/0_boss.js
+++ Priyansh/commands/0_boss.js
@@ -1,30 +1,26 @@
 const fs = require("fs");
-module.exports = {
-  config:{
-	name: "Boss",
-        version: "1.0.1",
-        prefix: false,
-	permssion: 0,
-	credits: "Krishna", 
-	description: "Fun",
-	category: "no prefix",
-	usages: "boss",
-        cooldowns: 5, 
-},
+module.exports.config = {
+	name: "prem aa gya",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "riya", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "Prem",
+    cooldowns: 5, 
+};
 
-handleEvent: function({ api, event, client, __GLOBAL }) {
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-  const content = event.body ? event.body : '';
-  const body = content.toLowerCase();
-	if (body.indexOf("malik")==0 || body.indexOf("Malik")==0 || body.indexOf("Ravi")==0 || body.indexOf("krishan")==0) {
+	if (event.body.indexOf("krishna")==0 || event.body.indexOf("Krishna")==0 || event.body.indexOf("KRISHNA")==0 || event.body.indexOf("@ᴹʳ᭄ꔰ『Krishna』 ꔰ鬱࿐")==0) {
 		var msg = {
 				body: "💝🌸 𝐎𝐖𝐍𝐄𝐑 ÷ ☞︎ 💝 𝐌𝐑. 𝗞ᏒƖ𝘚Η𝑵𝔸 💝 ☜︎\n✧═════════•❁❀❁•═════════✧\n☞︎ 💝🌸🍒 मेरे बाबू कृष्णा की प्रोफ़ाइल 💝🍒🌸 ☜︎\n✧═════════•❁❀❁•═════════✧\n☞︎ 🍒🌸 मेरे मालिक की फेसबुक की लिंक 💝🌸🍒 ☜︎\n✧═════════•❁❀❁•═════════✧\n☞︎  [ https://www.facebook.com/profile.php?id=61568216591260&mibextid=ZbWKwL ] ☜︎",
-				attachment: fs.createReadStream(__dirname + `/noprefix/boss.jpg`)
+        attachment: fs.createReadStream(__dirname + `/noprefix/boss.jpg`)
 			}
-			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🫅🏻", event.messageID, (err) => {}, true)
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
 		}
-	},
-	module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+	}
